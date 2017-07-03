ab03b13640f6805b074f053c29189606c4ad0094
nikkollaii <79139978241@mail.ru>
2017-07-03 16:22:28 +0700

    error stream

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -5,10 +5,7 @@ import computationalModel.line.CMLine;
 import computationalModel.tree.CMTree;
 import computationalModel.tree.CMTreeVertex;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
@@ -215,6 +212,15 @@ public class Parser {
             while (pr.isAlive()) {
                 try {
                     pr.waitFor();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(pr.getErrorStream()));
+                    String inputLine = in.readLine();
+                    if (inputLine != null) {
+                        logCollector.addLine("Error stream ");
+                        while ((inputLine = in.readLine()) != null) {
+                            logCollector.addLine(inputLine);
+                        }
+                        logCollector.addLine("");
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                     return false;
