29927869f42c9a752d5c478910cb2160effede67
Anastasia <prokopyeva96@yandex.ru>
2017-07-04 15:21:11 +0700

    little change

--- src/parser/performing/PerformUnix.java
+++ src/parser/performing/PerformUnix.java
@@ -11,12 +11,15 @@ public class PerformUnix extends Perform {
 
     @Override
     Process start(CMLine line) throws IOException {
+        /*
+        *  Первый аргумент возможен "sh", "csh"
+        * */
         ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", line.getCommand());
         return processBuilder.start();
     }
 
     public static void main(String[] args) throws IOException {
-        String s = "file_1 file_2;out.txt;echo \"Hello world\" >> out.txt";
+        String s = "file_1 file_2;out.txt;echo Hello world > out.txt";
         CMLine cmLine = new CMLine(s, null);
         PerformUnix performUnix = new PerformUnix();
         performUnix.start(cmLine);
